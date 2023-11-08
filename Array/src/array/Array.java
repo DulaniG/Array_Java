be3b84e36279b243a340f05c80f8aa5ac389ac88
DulaniG <dulanigalanigamage9814@gmail.com>
2023-11-09 00:46:06 +0530

    Add Array_Java

---
+++ Array/src/array/Array.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package array;
+
+/**
+ *
+ * @author HP
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        int[] numbers = {1,2,3,4,5};
+//        String[] names = {"ann","chris","paul","robert"};
+//        
+//        System.out.println(numbers[]);
+        
+        int[][] numbers = { {1,2,3},{4,5,6},{7,8,9} };
+        
+        System.out.println(numbers[2][2]);
+    }
+    
+}
